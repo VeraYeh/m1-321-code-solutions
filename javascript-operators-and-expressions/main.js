20db65133e6cbd96a647a1433e522161e5ec944a
Vera Yeh <brisa8281@gmail.com>
2021-03-27 12:45:20 -0700

    Completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 5;
+var height = 8;
+var area = width * height;
+console.log('value of area: ', area);
+console.log('typeof area: ', typeof area);
+
+var bill = 100;
+var payment = 120;
+var change = payment - bill;
+console.log('value of change: ', change);
+console.log('typeof change: ', typeof change);
+
+var quizzes = 80;
+var midterm = 85;
+var final = 88;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade);
+console.log('typeof grade: ', typeof grade);
+
+var firstName = 'Hsunchih';
+var lastName = 'Yeh';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName: ', fullName);
+console.log('typeof fullName: ', typeof fullName);
+
+var pH = 3;
+var isAcidic = pH < 7;
+console.log('value of isAcidic: ', isAcidic);
+console.log('typeof isAcidic: ', typeof isAcidic);
+
+var headCount = 12;
+var isSparta = headCount === 300;
+console.log('value of isSparta: ', isSparta);
+console.log('typeof isSparta: ', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('value of motto: ', motto);
+console.log('typeof motto: ', typeof motto);
