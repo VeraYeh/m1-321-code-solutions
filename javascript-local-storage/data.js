55b2f6baae361a2675a19e8ede5efb0f8d54c7f5
Vera Yeh <brisa8281@gmail.com>
2021-05-08 13:18:38 -0700

    Completed javascript-local-storage.

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,9 @@
+/* exported todos */
+
+var todos = [];
+
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
