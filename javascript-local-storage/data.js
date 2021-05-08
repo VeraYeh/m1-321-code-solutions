3ff3640f4cebfbc36d297fb4e344805400463e4c
Vera Yeh <brisa8281@gmail.com>
2021-05-08 13:28:56 -0700

    Moved event listener to data.js file.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -2,6 +2,11 @@
 
 var todos = [];
 
+window.addEventListener('beforeunload', function (event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+});
+
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 
 if (previousTodosJSON !== null) {
--- javascript-local-storage/main.js
+++ javascript-local-storage/main.js
@@ -71,8 +71,3 @@ function renderTodo(todo) {
 
   return $todo;
 }
-
-window.addEventListener('beforeunload', function (event) {
-  var todosJSON = JSON.stringify(todos);
-  localStorage.setItem('javascript-local-storage', todosJSON);
-});
